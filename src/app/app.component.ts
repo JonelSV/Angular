91e055a9cdfeb4af24eca4aa8a5699a8503065b6
JonelSV <svjonel@gmail.com>
2021-10-14 21:03:01 -0400

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myAngularProject';
+  title = 'My First Angular Project';
+
+  firstName = 'Jonel';
+  lastName = 'San Vicente';
 }

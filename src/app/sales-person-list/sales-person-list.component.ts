3242d65c3fc354876e9e8b601343b419e8aa8cfa
JonelSV <svjonel@gmail.com>
2021-10-14 21:36:34 -0400

    commit

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
+
+  // create an array of sales peerson objects
+
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Boris", "Medvedev", "boris@sales.com", 8000),
+    new SalesPerson("Michael", "Clayton", "clayton@sales.com", 4000),
+    new SalesPerson("Harris", "Moore", "harris@sales.com", 7000),
+    new SalesPerson("Trevor", "Jackson", "trevor@sales.com", 6000),
+  ];
+
 
   constructor() { }
 
